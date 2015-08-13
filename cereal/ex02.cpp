e4ad80e98f4f97b5ac20ba21ac7e27d45f34f6d3
DavadDi <dwh0403@163.com>
2015-08-13 15:30:45 +0800

    update

--- cereal/ex02.cpp
+++ cereal/ex02.cpp
@@ -1,6 +1,18 @@
 #include <cereal/archives/binary.hpp>
 #include <sstream>
 
+struct MyData
+{
+      int x, y, z;
+
+        // This method lets cereal know which data members to serialize
+        template<class Archive>
+        void serialize(Archive & archive)
+        {
+            archive( x, y, z  ); // serialize things by passing them to the archive
+        }
+ };
+
 int main()
 {
     std::stringstream ss; // any stream can be used
