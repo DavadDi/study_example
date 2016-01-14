cc2ca2830a930a1b3a40be03c43ef0e459376561
DavadDi <dwh0403@163.com>
2016-01-14 15:15:17 +0800

    update

---
+++ msgpack_flatbuffer_compare/fb/start_perf.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+echo "time ./fb_benchmark"
+echo -n "--------------------"
+time ./fb_benchmark
+
+
+echo "time ./fb_benchmark_O1"
+echo -n "--------------------"
+time ./fb_benchmark_O1
+
+
+echo "time ./fb_benchmark_O2"
+echo -n "--------------------"
+time ./fb_benchmark_O2
+
+
+echo "time ./fb_benchmark_O3"
+echo -n "--------------------"
+time ./fb_benchmark_O3
+
---
+++ msgpack_flatbuffer_compare/mp/start_perf.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+echo "time ./mp_benchmark"
+echo -n "--------------------"
+time ./mp_benchmark
+
+
+echo "time ./mp_benchmark_O1"
+echo -n "--------------------"
+time ./mp_benchmark_O1
+
+
+echo "time ./mp_benchmark_O2"
+echo -n "--------------------"
+time ./mp_benchmark_O2
+
+
+echo "time ./mp_benchmark_O3"
+echo -n "--------------------"
+time ./mp_benchmark_O3
+
